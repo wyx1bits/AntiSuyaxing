94125383ad5fccf19805be4223c85a5a94229473
YXwang <wangyixuan_14@outlook.com>
2026-09-13 17:24:36 +0800

    将提权部分写入1.1版本

--- 程序汇总.bat
+++ 程序汇总.bat
@@ -1,1 +1,15 @@
 
+@echo off
+set "PSexecPath=D:\PSTools\psexec.exe"
+set "ExpectedArgs=-i -s cmd.exe"
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    pause
+    exit /b
+)
+if not exist "%PSexecPath%" (
+    pause
+    exit /b
+)
+start "" "%PSexecPath%" %ExpectedArgs%
+exit
